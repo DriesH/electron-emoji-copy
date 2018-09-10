1b30d0f5e8938d5491c5ae52c4d7fc4f50f042cc
Dries Heyninck <dries.heyninck@gmail.com>
2018-09-10 23:26:57 +0200

    Added different start scripts

--- src/electron-main-process.js
+++ src/electron-main-process.js
@@ -10,7 +10,7 @@ let mainWindow;
 function createWindow() {
   mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
-  mainWindow.loadURL('http://localhost:3000');
+  mainWindow.loadURL('http://localhost:3001');
 
   mainWindow.webContents.openDevTools();
 
